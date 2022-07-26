93e5f72b491636df8d7600b4c59bac869f46a451
jsebastianbox <juanse.lopez@bussbox.com>
2022-07-26 18:50:01 -0300

    nodemailer agregado bien!

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -5,9 +5,11 @@ import {io} from "../app.js"
 import { __dirname } from "../app.js"
 import passport from 'passport'
 import dotenv from "dotenv"
+import nodemailer from "nodemailer"
 dotenv.config()
 const router = express.Router()
 
+/* session config */
 router.use(session({
     saveUninitialized:false,
     resave:false,
@@ -20,19 +22,49 @@ router.use(session({
         maxAge : 10000
     }
 }))
-
+/* nodemailer config */
+const TEST_EMAIL = 'juanselopezg05@gmail.com'
+const TEST_PASSWORD = 'sztgquwhkltsnxpe'
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com', //host de la cuenta origen
+    port: 587,
+    auth: {
+        user: TEST_EMAIL,
+        pass: TEST_PASSWORD,
+    }
+})
+/* email structure  */
+const email_structure = {
+    from: 'Server nodejs',
+    to: TEST_EMAIL,
+    subject: 'Test: Sign up',
+    html: `
+    <p>Your sign up was succesfully completed bro!</p><br>
+    <p>Juan Lopez - Coder House student</p>
+    `,
+}
+/* routes */
 router.get('/', (req, res) => {
     let user = req.session.user
     if(user) return res.redirect('/logged')
     res.sendFile('/public/html/signup.html', { root: __dirname })
 })
-router.post('/', passport.authenticate('signup', {failureRedirect:'/'}),(req, res) => {
+
+router.post('/', passport.authenticate('signup', {failureRedirect:'/'}), async (req, res) => {
     let user = req.body
     req.session.user = user
     io.on('connection', socket => {
         socket.emit('user', user)
     })
-    res.redirect('/logged')
+    /* send mail post */
+    try {
+        let info =  await transporter.sendMail(email_structure)
+        console.log(info)
+        res.redirect('/logged')
+    } catch (error) {
+        res.send(`Email unable to send to ${TEST_EMAIL}`)
+        console.log(error)
+    }
 })
 
 export default router
