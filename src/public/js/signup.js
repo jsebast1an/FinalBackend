d593e6c896067c36f45d85fd7785483826b77309
jsebastianbox <juanse.lopez@bussbox.com>
2022-09-25 22:05:43 -0300

    BG para signup y header completado, inicio pug js

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -11,15 +11,19 @@ signupForm.addEventListener('submit', (e) => {
     let messages = []
     if (username.value == '' || username.value.length < 4) {
         messages.push('Full name require more than 4 characters')
+        username.style.backgroundColor = '#ffcccb'
     }
     if (phone.value == '' || phone.value.length < 4) {
         messages.push('Phone require more than 4 characters')
+        phone.style.backgroundColor = '#ffcccb'
     }
     if (email.value == '' || email.value.length < 4) {
         messages.push('Email require more than 4 characters')
+        email.style.backgroundColor = '#ffcccb'
     }
     if (password.value == '' || password.value.length < 4) {
         messages.push('Password require more than 4 characters')
+        password.style.backgroundColor = '#ffcccb'
     }
     // verify password
     if (password.value != check_password.value) {
@@ -28,7 +32,15 @@ signupForm.addEventListener('submit', (e) => {
     
     if(messages.length > 0) {
         e.preventDefault()
-        errors.style.color = 'red'
-        errors.innerText = messages.join(', ')
+        errors.style.color = '#ff534f'
+        errors.innerText = messages.join('. ')
     }
 })
+
+$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+	
+$("#menu-icon").on("click", function(){
+$("nav").slideToggle();
+$(this).toggleClass("active");
+});
+
